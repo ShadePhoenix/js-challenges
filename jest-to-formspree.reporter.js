b6b61912fb6c6ba78f280d0b676aefafc84363c2
Daniel Legaspi-Finch <shadow_cloud_master@hotmail.com>
2022-09-05 10:41:45 +0930

    Updated names for new files, changes outputs submitted to Formspree

---
+++ jest-to-formspree.reporter.js
@@ -0,0 +1,56 @@
+const { default: axios } = require("axios");
+
+class JestFormspreeReporter {
+    constructor(globalConfig, options) {
+        this._globalConfig = globalConfig;
+        this._options = options;
+    }
+
+    onRunComplete(contexts, results) {
+        console.log("\nFailed Test Suites: ", results.numFailedTestSuites);
+        console.log("Passed Test Suites: ", results.numPassedTestSuites);
+        console.log("Failed Tests: ", results.numFailedTests);
+        console.log("Passed Tests ", results.numPassedTests);
+        const failedTests = results.testResults
+            .map((suites) =>
+                suites.testResults
+                    .filter((tests) => tests.status === "failed")
+                    .map((iTests) => iTests.fullName)
+            )
+            .filter((s) => s.length > 0);
+        console.log("Failed Test Messages", failedTests);
+
+        if (!process.env.USER) {
+            return new Error(
+                "There is no user set in environment. No submission will be made to Formspree."
+            );
+        }
+
+        axios({
+            url: "https://formspree.io/f/xnqrnkll",
+            method: "post",
+            headers: {
+                Accept: "application/json"
+            },
+            data: {
+                name: process.env.USER,
+                results: {
+                    FailedTests: results.numFailedTests,
+                    PassedTests: results.numPassedTests,
+                    TotalTests: results.numTotalTests,
+                    FailedTestMessages: failedTests
+                }
+            }
+        }).then((response) => {
+            console.log(response);
+        });
+    }
+
+    getLastError() {
+        if (this._shouldFail) {
+            console.log("There were failed tests");
+        }
+    }
+}
+
+module.exports = JestFormspreeReporter;
